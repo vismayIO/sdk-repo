4e271965c58866444e82d175d5bb4927439fd098
vismayIO <garygaming6352@gmail.com>
2026-02-09 12:19:34 +0530

    skills

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,26 @@
-import { Suspense } from "react";
-import { Button } from "./components/button";
+import { initializeDuckDb } from "@sdk-repo/sdk/duck-db";
 import "@sdk-repo/sdk/styles.css";
+import { useEffect } from "react";
+import { Button } from "./components/button";
 
 function App() {
+  useEffect(() => {
+    const config = {
+      query: {
+        /**
+         * By default, int values returned by DuckDb are Int32Array(2).
+         * This setting tells DuckDB to cast ints to double instead,
+         * so they become JS numbers.
+         */
+        castBigIntToDouble: true,
+      },
+    };
+    initializeDuckDb({ config, debug: true });
+  }, []);
+
   return (
     <>
-      <Suspense fallback="Loading...">
-        <Button />
-      </Suspense>
+      <Button />
     </>
   );
 }
